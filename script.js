1e4f3d08867bf8ee8dd04f8d224252538c99fb54
Akintunde Oyelakin <113299215+CodewithOye@users.noreply.github.com>
2023-10-09 10:43:01 -0700

    new portfolio slide show

--- script.js
+++ script.js
@@ -97,3 +97,24 @@ document.addEventListener('scroll', (e) => {
     }
 
 });
+
+
+const swiper = new Swiper('.swiper', {
+    autoplay:{
+      delay:3000,
+      disableOnInteraction: false ,
+
+    },
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable :true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
